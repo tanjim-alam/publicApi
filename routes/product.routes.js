2045d67f2d2c41542af1bde201a72e952b20e696
Tanjim Alam <122804862+Tanjim099@users.noreply.github.com>
2024-05-08 01:18:48 +0530

    updated

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { createProduct, deleteProduct, getAllProducts, updateProduct } from "../controllers/product.controller.js";
 
-const productRoutes = new Router();
+const productRoutes = Router();
 productRoutes.post("/", createProduct);
 productRoutes.get("/", getAllProducts);
 productRoutes.put("/:pid", updateProduct);
